7a68c5423c5b6cc9f98ed3057f957848d10597e7
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-04-14 12:05:07 -0300

    add rota certificado, e funcionalidade para gerar um certificado
    Nesta rota esta opções para o usuário fazer download de um certificado que está no disponível.
    Foi necessário converter as informações para html para poder gerar um certificado

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const CryptoJS = require("crypto-js");
+const pdf = require('html-pdf');
+const ejs = require('ejs');
+
 const express = require('express');
-
 const cors = require('cors');
-
 const app = express();
+const path = require('path');
 
 app.listen('5000', () => console.log('Porta 5000 disponível'));
 
@@ -41,4 +44,83 @@ app.route('/certificado').get((req, res) => {
   });
 
   res.json(pegarDados);
+});
+
+app.route('/certificado').post((req, res) => {
+  const { id, curso, timestamp, dataEmissao } = req.body;
+
+  const certificado = certificados.find((certificado) => certificado.id === id)
+
+  if(certificado){
+
+    const hash = { 
+      id, 
+      timestamp 
+    };
+
+    const encriptar = CryptoJS.AES.encrypt(JSON.stringify(hash), "Secret Passphrase").toString();
+
+    const atualizarCertificado = {
+      ...certificado,
+      timestamp,
+      dataEmissao,
+      hash: encriptar
+    };
+    
+    certificados = certificados.map((certificado) => {
+      if(certificado.id === id){
+        certificado = atualizarCertificado;
+      }
+
+      return certificado;
+    });
+
+    const dataEjs = {
+      id: id,
+      time: timestamp,
+      nome: certificado.name,
+      curso: certificado.curso,
+      horas: certificado.quantidadeHoras,
+      data: dataEmissao,
+      hash: encriptar
+    }
+
+    ejs.renderFile(
+      path.join(__dirname + '/certification.ejs'), 
+      dataEjs, 
+      (err, html) => {
+        if(err){
+          return res.send(err);
+        }
+
+        const optionsPdf = { 
+          "format": "A3",        
+          "orientation": "landscape",
+          "border": {
+            "top": "32px",            
+            "right": "32px",
+            "bottom": "32px",
+            "left": "32px"
+          },
+        };
+          
+        pdf.create(html, optionsPdf)
+        .toFile(
+          path.join(__dirname + '/download' + `/certificado${curso}.pdf`), 
+          (err, filepath) => {
+            if(err){
+              return res.json(err);
+            }
+
+            res.type('pdf')
+            return res.download(path.join(__dirname + '/download' + `/certificado${curso}.pdf`))
+          }
+        )
+    });
+  }
+  
+  else{
+    return res.json('Certificado não existe');
+  }
+
 });
