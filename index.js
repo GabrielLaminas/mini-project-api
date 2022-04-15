c86fea251cc1f13011497b3957a1396caa8312e9
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-04-15 17:49:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Porta ${port} disponível`));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", 'GET,POST');
   app.use(cors());
   next();
 });
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Porta ${port} disponível`));
-
-app.use(express.json());
 
 /*
 fs.mkdirSync('upload/', { recursive: true}, (err) => {
