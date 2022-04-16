df14f46ea9d714a920593865bbd5466b640119e1
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-04-16 11:20:50 -0300

    text permitindo cors

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.options('*', cors());
 
 app.use(express.json());
 /*
@@ -67,9 +66,6 @@ app.route('/listagem').get((req, res) => {
 });
 
 app.route('/certificado').post((req, res) => {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000/');
-  res.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-
   const { id, curso, timestamp, dataEmissao } = req.body;
 
   const certificado = certificados.find((certificado) => certificado.id === id)
@@ -211,4 +207,21 @@ app.post('/validacao', upload.single('pdf'), async (req, res) => {
 
 app.listen(port, () => console.log(`Porta ${port} disponível`));
 
-module.exports = app;
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+module.exports = allowCors(app);
